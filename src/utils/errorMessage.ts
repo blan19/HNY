6220947985df20d3c4c6066151794c2da71c76af
Junseo <oponize@naver.com>
2023-01-16 23:55:00 +0900

    feat: Error Message enum 추가

---
+++ src/utils/errorMessage.ts
@@ -0,0 +1,8 @@
+enum ErrorMessage {
+  isNotHTMLElementMsg = "타겟은 HTMLElement 인스턴스",
+  isNotPropertyMsg = "프로퍼티가 존재하지 않습니다",
+  isAlreadyRegisteredRefMsg = "이미 등록된 ref",
+  isEmptyStringMsg = "빈 문자열",
+}
+
+export default ErrorMessage;
