649f171e7ae648f179dd09ae2e86df314c49fe95
박준서 <oponize@naver.com>
2023-01-17 16:24:59 +0900

    feat: Router 추가

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,60 @@
-export {};
+import HomePage from "./page/Home";
+import PostingPage from "./page/Posting";
+import CreatePage from "./page/Create";
+import NotFound from "./page/404";
+import Router from "./utils/Router";
+import HTMLElementViewModel from "./core/HTMLElementViewModel";
+
+const html = /* html */ `
+<main>
+  <h1>Hello World..!</h1>
+</main>
+
+<style scoped>
+:host {
+  display: block;
+}
+</style>
+`;
+
+const App = class {
+  constructor() {
+    window.customElements.define(
+      "hny-app",
+      class extends HTMLElementViewModel<{}> {
+        constructor() {
+          super({
+            html,
+          });
+        }
+      }
+    );
+
+    new Router({
+      routes: [
+        {
+          path: "/",
+          component: HomePage,
+        },
+        {
+          path: "/posting/:id",
+          component: PostingPage,
+        },
+        {
+          path: "/create",
+          component: CreatePage,
+        },
+        {
+          path: "/*",
+          component: NotFound,
+        },
+      ],
+    });
+  }
+};
+
+window.onload = () => {
+  new App();
+};
+
+export default App;
--- src/utils/Router.ts
+++ src/utils/Router.ts
@@ -1,20 +1,100 @@
+import Page from "../core/Page";
+import NotFound from "../page/404";
+import ErrorMessage from "./errorMessage";
+import { el } from "./lib";
+
+interface Route {
+  path: string;
+  component: typeof Page;
+}
+
 interface RouterConstructor {
-  routes: {
-    path: string;
-    component: any;
-  }[];
+  routes: Route[];
 }
 
 const Router = class {
-  routes;
+  #routes;
 
   constructor({ routes: __routes }: RouterConstructor) {
-    this.routes = __routes;
+    this.#routes = __routes;
+
+    this.#bind();
+    this.#routing();
   }
 
-  init() {}
+  static push(url: string, options?: CustomEventInit<unknown>) {
+    const event = new CustomEvent("CHANGE_ROUTE", options);
+    history.pushState(null, "", url);
+    window.dispatchEvent(event);
+  }
 
-  setup() {}
+  static pushBack(options?: CustomEventInit<unknown>) {
+    const event = new CustomEvent("CHANGE_ROUTE", options);
+    history.back();
+    window.dispatchEvent(event);
+  }
+
+  static replace(url: string, options?: CustomEventInit<unknown>) {
+    const event = new CustomEvent("CHANGE_ROUTE", options);
+    history.replaceState(null, "", url);
+    window.dispatchEvent(event);
+  }
+
+  #routing(): void {
+    const root = el("hny-app");
+    const notFound =
+      this.#routes.find((route) => route.path === "/*")?.component || NotFound;
+
+    if (!root) throw Error(ErrorMessage["isNotHTMLElementMsg"]);
+
+    root.innerHTML = "";
+
+    const [, ...currentPath] = window.location.pathname.split("/");
+
+    const target = this.#routes.filter(
+      (route) => route.path.split("/").slice(1).length === currentPath.length
+    );
+
+    if (target.length < 1) {
+      new Page({ target: root, component: notFound });
+      return;
+    }
+
+    let currectRoute: Route | null = null;
+
+    for (const route of target) {
+      const [, ...path] = route.path.split("/");
+
+      if (currentPath.length !== path.length) continue;
+
+      const isCurrectPath = currentPath.every((value, index) => {
+        const isParams = path[index].startsWith(":");
+
+        if (isParams) return true;
+
+        return value === path[index];
+      });
+
+      if (isCurrectPath) {
+        currectRoute = route;
+        break;
+      }
+    }
+
+    if (!currectRoute) {
+      new Page({ target: root, component: notFound });
+      return;
+    }
+
+    new Page({ target: root, component: currectRoute.component });
+    return;
+  }
+
+  #bind() {
+    window.addEventListener("CHANGE_ROUTE", () => {
+      this.#routing();
+    });
+  }
 };
 
 export default Router;
