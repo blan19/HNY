6c376d5501464d44bf2cda335aa9e93ad8179b73
박준서 <oponize@naver.com>
2023-01-18 17:28:54 +0900

    feat: getErrorMessage Util 추가

--- src/utils/errorMessage.ts
+++ src/utils/errorMessage.ts
@@ -3,6 +3,18 @@ enum ErrorMessage {
   isNotPropertyMsg = "프로퍼티가 존재하지 않습니다",
   isAlreadyRegisteredRefMsg = "이미 등록된 ref",
   isEmptyStringMsg = "빈 문자열",
+  isApiError = "",
 }
 
+const HNYApiError = class extends Error {
+  status: number;
+  constructor(status: number, message: string) {
+    super();
+    this.status = status;
+    this.message = message;
+    this.name = "HNY-Api-Error";
+  }
+};
+
+export { HNYApiError };
 export default ErrorMessage;
--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -2,6 +2,31 @@ import ErrorMessage from "./errorMessage";
 
 type CustomHTMLElementTagNameMapKey = keyof HTMLElementTagNameMap | "hny-app";
 
+type ErrorWithMessage = {
+  message: string;
+};
+
+const isErrorWithMessage = (error: unknown): error is ErrorWithMessage => {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "message" in error &&
+    typeof (error as Record<string, unknown>).message === "string"
+  );
+};
+
+const toErrorWithMessage = (maybeError: unknown): ErrorWithMessage => {
+  if (isErrorWithMessage(maybeError)) return maybeError;
+
+  try {
+    return new Error(JSON.stringify(maybeError));
+  } catch {
+    return new Error(String(maybeError));
+  }
+};
+
+const getErrorMessage = (error: unknown) => toErrorWithMessage(error).message;
+
 const el = (
   selector: CustomHTMLElementTagNameMapKey,
   element?: Element | Document
@@ -15,4 +40,4 @@ const el = (
   return element.querySelector(selector);
 };
 
-export { el };
+export { el, getErrorMessage };
