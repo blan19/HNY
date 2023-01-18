a5affd42dc00f8abacc331f407d197f0cd9903fd
Junseo <oponize@naver.com>
2023-01-19 03:04:45 +0900

    feat: 환경변수 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig({
-  plugins: [EnvironmentPlugin(["VITE_BASE_URL"])],
+  plugins: [
+    EnvironmentPlugin([
+      "VITE_BASE_URL",
+      "VITE_UNSPLASH_ACCESS_KEY",
+      "VITE_UNSPLASH_SECRET_KEY",
+    ]),
+  ],
 });
