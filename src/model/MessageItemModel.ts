8f3c57eb76dcdb75a24a06e94939df9c6bc17e6e
Junseo <oponize@naver.com>
2023-01-18 19:32:37 +0900

    chore: MessageItemModel 필요 없는 프롭스 제거

--- src/model/MessageItemModel.ts
+++ src/model/MessageItemModel.ts
@@ -7,34 +7,26 @@ const MessageItemModel = class {
   #title: string;
   #content: string;
   #image: string;
-  #createdAt: string;
-  #updatedAt: string;
   constructor({
     postId,
     title,
     content,
     image,
-    createdAt,
-    updatedAt,
-  }: Message) {
+  }: Omit<Message, "creatAt" | "updateAt">) {
     this.#postId = postId;
     this.#title = title;
     this.#content = content;
     this.#image = image;
-    this.#createdAt = createdAt;
-    this.#updatedAt = updatedAt;
   }
 
   generateElement(): Element {
     const messageItem = document.createElement("hny-message-item");
 
-    messageItem.setAttribute("m-data-postId", this.#postId);
+    messageItem.setAttribute("m-data-post-id", this.#postId);
     messageItem.setAttribute("m-data-title", this.#title);
     messageItem.setAttribute("m-data-content", this.#content);
     messageItem.setAttribute("m-data-image", this.#image);
     messageItem.setAttribute("m-data-content", this.#content);
-    messageItem.setAttribute("m-data-createAt", this.#createdAt);
-    messageItem.setAttribute("m-data-updateAt", this.#updatedAt);
 
     return messageItem;
   }
