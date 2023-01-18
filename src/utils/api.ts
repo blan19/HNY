d0961d93834e496c189aeb6b23bed4dca865281a
박준서 <oponize@naver.com>
2023-01-18 17:28:32 +0900

    feat: api method 추가

---
+++ src/utils/api.ts
@@ -0,0 +1,67 @@
+import { HNYApiError } from "./errorMessage";
+import { getErrorMessage } from "./lib";
+
+interface FetcherRequestInit extends RequestInit {
+  params?: number | string;
+  query?: {
+    [key: string]: any;
+  };
+  data?: unknown;
+}
+
+enum Api {
+  baseUrl = "http://43.201.103.199",
+}
+
+const fetcher = (url: string, options: FetcherRequestInit) => {
+  const { params, query, ...rest } = options;
+
+  if (params) url += `/${params}`;
+  if (query) url += `?${new URLSearchParams(query)}`;
+
+  const response = fetch(`${Api["baseUrl"]}${url}`, rest);
+
+  return response;
+};
+
+const clientFetcher = async (url: string, options: FetcherRequestInit) => {
+  try {
+    const response = await fetcher(url, options);
+
+    if (!response.ok)
+      throw new HNYApiError(response.status, `${url} Api Error`);
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    throw new Error(getErrorMessage(error));
+  }
+};
+
+const clientApi = async (url: string, options: FetcherRequestInit) => {
+  const { data, headers, ...rest } = options;
+  const requestBody = JSON.stringify(data);
+
+  try {
+    const response = await fetcher(url, {
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+      body: requestBody,
+      ...rest,
+    });
+
+    if (!response.ok)
+      throw new HNYApiError(response.status, `${url} Api Error`);
+
+    const result = await response.json();
+
+    return result;
+  } catch (e) {
+    throw new Error(getErrorMessage(e));
+  }
+};
+
+export { Api, clientApi, clientFetcher };
